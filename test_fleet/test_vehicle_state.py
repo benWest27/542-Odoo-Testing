895366f88e802d0d77d0b541240c758c888d7bec
Anh Gia <gianguyenanhuy@gmail.com>
2024-10-26 20:15:03 -0700

    Create test_vehicle_state.py

---
+++ test_fleet/test_vehicle_state.py
@@ -0,0 +1,15 @@
+from odoo.tests import common
+
+class TestFleet(common.TransactionCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestFleet, cls).setUpClass()
+        cls.state_1 = cls.env['fleet.vehicle.state'].create({
+            'name': 'Operational',
+            'sequence': 10,
+        })
+
+    def test_vehicle_state_creation(self):
+        self.assertEqual(self.state_1.name, 'Operational', "The state name should be 'Operational'")
+        self.assertEqual(self.state_1.sequence, 10, "The sequence should be 10")
