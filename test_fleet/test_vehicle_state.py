afba1f76b8f8016be0ceea7a9c88b67fa853db12
Anh Gia <gianguyenanhuy@gmail.com>
2024-10-27 11:11:07 -0700

    Update test_vehicle_state.py

--- test_fleet/test_vehicle_state.py
+++ test_fleet/test_vehicle_state.py
@@ -13,3 +13,10 @@ class TestFleet(common.TransactionCase):
     def test_vehicle_state_creation(self):
         self.assertEqual(self.state_1.name, 'Operational', "The state name should be 'Operational'")
         self.assertEqual(self.state_1.sequence, 10, "The sequence should be 10")
+
+    def test_unique_state_name(self):
+        with self.assertRaises(Exception, msg="State name should be unique"):
+            self.env['fleet.vehicle.state'].create({
+                'name': 'Operational',  
+                'sequence': 20,
+        })
