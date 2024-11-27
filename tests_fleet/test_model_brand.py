565744fb58e9d2f6550c2efff445070264cf4a9b
benWest27 <benjamin_j_west27@csu.fullerton.edu>
2024-11-27 10:15:40 -0800

    Update test_model_brand.py
    changed the fleet brand model test to use a non existant brand and model so that it is unlikely to run into an already exists error

--- tests_fleet/test_model_brand.py
+++ tests_fleet/test_model_brand.py
@@ -6,15 +6,15 @@ class TestFleet(common.TransactionCase):
     def test_create_vehicle_brand_and_model(self):
         # Create a brand
         brand = self.env["fleet.vehicle.model.brand"].create({
-            "name": "Tesla",
+            "name": "Nickolas",
         })
-        self.assertEqual(brand.name, "Tesla", "The vehicle brand name should be 'Tesla'")
+        self.assertEqual(brand.name, "Nickolas", "The vehicle brand name should be 'Nickolas'")
 
         # Create a model linked to the brand
         model = self.env["fleet.vehicle.model"].create({
             "brand_id": brand.id,
-            "name": "Model S",
+            "name": "Model screq",
             "vehicle_type": "car",
         })
         self.assertEqual(model.brand_id, brand, "The vehicle model should be linked to the correct brand")
-        self.assertEqual(model.name, "Model S", "The vehicle model name should be 'Model S'")
+        self.assertEqual(model.name, "Model screq", "The vehicle model name should be 'Model screq'")
